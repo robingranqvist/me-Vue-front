8bc73a09851fa74ef3d2782a850ffce0203d7e7e
Robin Granqvist <54509721+robingranqvist@users.noreply.github.com>
2020-09-20 04:58:26 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+		<!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+		<Nav />
+		<div class="main-content">
+			<router-view></router-view>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+		// HelloWorld
+		Nav
+	}
 }
 </script>
 
 <style>
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	transition: all 150ms ease-in-out;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #333;
+	max-width: 700px;
+	margin: 0 auto;
+	margin-top: 50px;
+	/* text-align: center; */
+}
+
+.main-content {
+	margin-top: 50px;
+}
+
+h1 {
+	margin-bottom: 50px;
+}
+
+a {
+	color: #333;
+	display: block;
+	text-decoration: none;
+}
+
+a:hover {
+	opacity: 0.5;
+}
+
+.input {
+	width: 100%;
+	padding: 10px;
+	border-radius: 4px;
+	border: 1px solid #b3b1b1;
+	margin-bottom: 20px;
+}
+
+.btn {
+	width: 100%;
+	padding: 20px;
+	cursor: pointer;
+	background-color: #3498db;
+	color: #fff;
+	border: none;
+	border-radius: 4px;
+}
+
+.input:focus {
+	outline: none;
+}
+
+p {
+	line-height: 25px;
+}
+
+.finished {
+	text-align: center;
+	margin-top: 20px;
+	color: green;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false
+// Routes
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import NotFound from './pages/404';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+// // Weeks
+// import Week1 from './weeks/Week1';
+// import Week2 from './weeks/Week2';
+
+// Post
+import Post from './components/Post';
+
+Vue.use(VueRouter);
+
+const routes = [
+	{ path: '/', component: Home },
+	{ path: '/reports', component: Reports },
+	{ path: '/login', component: Login },
+	{ path: '/register', component: Register },
+	// Weeks
+	// { path: '/reports/week/1', component: Week1 },
+	// { path: '/reports/week/2', component: Week2 },
+	{ path: '/reports/week/:id', component: Post },
+	{ path: '*', component: NotFound }
+];
+
+const router = new VueRouter({
+	routes: routes,
+	mode: 'history'
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+	render: h => h(App),
+	router: router,
 }).$mount('#app')
